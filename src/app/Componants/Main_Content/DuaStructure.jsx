89ceb8b64bcbfe2cf98f9a96d2268757a3040362
safwan 169 <safwanhossainmridul@gmail.com>
2025-01-08 23:53:27 +0600

    add audio

--- src/app/Componants/Main_Content/DuaStructure.jsx
+++ src/app/Componants/Main_Content/DuaStructure.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+'use client'
+
+import { useRef } from "react";
 
 const DuaStructure = ({ data }) => {
-  console.log(data, "adfasdfdfasf");
-  return (
-    <div id={`${data.id}`} className="mb-2 bg-white text-left p-5 rounded-lg space-y-5">
+
+  console.log(data,'adfsf')
+  const audioRef = useRef(null);
+
+  const handlePlayAudio = () => {
+    if (audioRef.current.paused) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  };  return (
+    <div
+      id={`${data.id}`}
+      className="mb-2 bg-white text-left p-5 rounded-lg space-y-5"
+    >
       <h2 className="text-lg  flex items-center  gap-2 font-bold text-green-600 ">
         <img className="w-10 h-10" src="/duacard.svg" alt="duacard" />
         {data?.id}.{data?.name}
@@ -22,6 +36,16 @@ const DuaStructure = ({ data }) => {
       <div>
         <span className="text-green-600 font-semibold mr-1">Reference:</span>
         {data?.refference_en}
+      </div>
+      <div className={`${data?.audio?'block':'hidden'}`}>
+        <img
+          src="/audiobtn.svg" 
+          alt=" Icon"
+          title="Data Base given link is not working"
+          className="w-10 h-10 cursor-pointer rounded-full"
+          onClick={handlePlayAudio}
+        />
+        <audio ref={audioRef} src={`${data?.audio}`}></audio>{" "}
       </div>
     </div>
   );
